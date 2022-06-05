f0bac7be788d32cb061f2384d79741c5059125c8
maratgareev97 <maratgareev97@gmail.com>
2022-06-05 17:07:24 +0300

    my

--- src/main/java/web/service/CarServiseImpl.java
+++ src/main/java/web/service/CarServiseImpl.java
@@ -24,20 +24,21 @@ public class CarServiseImpl implements CarService {
 
     @Override
     public List<Car> listCar() {
-
-        Car car1 = new Car(1L, "Ваз", 1111);
-        Car car2 = new Car(2L, "Газ", 2222);
-        Car car3 = new Car(3L, "Паз", 3333);
-        Car car4 = new Car(4L, "Маз", 4444);
-        Car car5 = new Car(5L, "Заз", 5555);
-//        List<Car> listCars = new ArrayList<>();
-        listCars.add(car1);
-        listCars.add(car2);
-        listCars.add(car3);
-        listCars.add(car4);
-        listCars.add(car5);
-
-        return listCars;
+//
+//        Car car1 = new Car(1L, "Ваз", 1111);
+//        Car car2 = new Car(2L, "Газ", 2222);
+//        Car car3 = new Car(3L, "Паз", 3333);
+//        Car car4 = new Car(4L, "Маз", 4444);
+//        Car car5 = new Car(5L, "Заз", 5555);
+////        List<Car> listCars = new ArrayList<>();
+//        listCars.add(car1);
+//        listCars.add(car2);
+//        listCars.add(car3);
+//        listCars.add(car4);
+//        listCars.add(car5);
+//
+//        return listCars;
+        return null;
     }
 
     @Override
